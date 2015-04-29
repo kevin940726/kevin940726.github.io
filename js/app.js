230ab24920c9dc2204a4f76ff2a770c6cb3ee7f9
kevin940726 <kevin830726@gmail.com>
2015-04-29 17:32:09 +0800

    First commit using Skeleton.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,70 @@
-$('document').ready(function() {
-
-	$('.arrow-wrap').click(function() {
-		if ($('html, body').scrollTop() < 100) {
-			$('html, body').animate( {
-				scrollTop: $(".content").offset().top
-			}, 1000);
+$(document).ready(function() {
+	var topset = false;
+	$('#menuCircle').draggable({
+		containment: "body",
+		start: function( event, ui ) {
+			$(this).animate({
+				backgroundColor: 'rgba(0,0,0,0.8)',
+				borderColor: 'rgba(255,255,255,1.0)',
+				boxShadowColor: 'rgba(0,0,0,1.0)'
+			});
+		},
+		stop: function( event, ui ) {
+			if ($(this).offset().left+$(this).width()/2 > ($(window).width()/2)){
+				$(this).animate({
+					left: $(window).width()-$(this).width()-4,
+					easing: "easeInOutCubic"
+				}, 500);
+			}
+			else{
+				$(this).animate({
+					left: 0,
+					easing: "easeInOutCubic"
+				}, 500);
+			}
+			if ($('.navbar-list').position().top != $(this).position().top && topset){
+				$('.navbar-list').animate({
+					top: $('#menuCircle').position().top-2,
+					easing: "easeInOutCubic"
+				}, 500);
+				topset = true;
+			}
+			$(this).animate({
+				backgroundColor: 'rgba(0,0,0,0.6)',
+				borderColor: 'rgba(255,255,255,0.9)',
+				boxShadowColor: 'rgba(0,0,0,0.8)'
+			});		
 		}
-		else {
-			$('html, body').animate( {
-				scrollTop: $(".bg-wrapper").offset().top
+	});
+	$('#menuCircle').click(function(event) {
+		event.stopPropagation();
+		if (!topset){
+			$('.navbar-list').animate({
+				top: $('#menuCircle').position().top-2,
+				easing: "easeInOutCubic"
 			}, 500);
+			topset = true;
 		}
-		return false;
-		e.preventDefault();
+		else{
+			$('.navbar-list').animate({
+				top: 0,
+				easing: "easeInOutCubic"
+			}, 500);
+			topset = false;
+		}
 	});
-
-	$('a[href^="#"]').click(function() {
-		$("html, body").animate({
-			scrollTop: $(this.hash).offset().top
+	$('html').click(function() {
+		if (topset){
+			$('.navbar-list').animate({
+				top: 0,
+				easing: "easeInOutCubic"
+			}, 500);
+			topset = false;
+		}
+	});
+	$('.navbar-list a').click(function() {
+		$('html, body').stop().animate( {
+			scrollTop: $($(this).attr('href')).offset().top-50
 		}, 500);
-		return false;
-		e.preventDefault();
 	});
-
 });
