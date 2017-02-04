26319c47421d7e01a742c918c2c7c1da2820eecd
kevin940726 <kevin830726@gmail.com>
2017-02-04 22:22:35 +0800

    initial setup

---
+++ src/layouts/Homepage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import LatestPosts from '../../components/LatestPosts';
+
+const Homepage = () => (
+  <div>
+    {/* <LatestPosts/> */}
+  </div>
+);
+
+export default Homepage;
